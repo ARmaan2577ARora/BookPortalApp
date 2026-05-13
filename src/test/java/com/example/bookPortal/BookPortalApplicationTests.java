095e3ec0d9d2e06af41886c7f994a908878afa7e
ARmaan2577ARora <armaaninsanmsg11@gmail.com>
2026-05-13 14:20:03 +0530

    Initializing project

---
+++ src/test/java/com/example/bookPortal/BookPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
