a85c097ffd45e4b82969edc57cc207d2be0eab8f
bhavyagarg21 <bhavyagarg1517@gmail.com>
2026-05-14 15:38:12 +0530

    Add StoreRepo and StoreBookRepo

--- src/main/java/com/example/bookPortal/repository/StoreRepo.java
+++ src/main/java/com/example/bookPortal/repository/StoreRepo.java
@@ -3,6 +3,25 @@ package com.example.bookPortal.repository;
 import com.example.bookPortal.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
 
 public interface StoreRepo extends JpaRepository<Store, Integer> {
+    List<Store> findByStoreNameContainingIgnoreCase(String storeName);
+
+    List<Store> findByCityIgnoreCase(String city);
+
+    List<Store> findByStateIgnoreCase(String state);
+
+    List<Store> findByCountryIgnoreCase(String country);
+
+    Optional<Store> findByWebsite(String website);
+
+    boolean existsByWebsite(String website);
+
+    List<Store> findByRatingGreaterThanEqual(BigDecimal rating);
+
+    List<Store> findByRatingBetween(BigDecimal minRating, BigDecimal maxRating);
 }
