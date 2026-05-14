4de130e42d525566ae278aa83361ca0e91e9bc1c
Akshant nain <aknain19@gmail.com>
2026-05-14 16:03:25 +0530

    Order Method

--- src/main/java/com/example/bookPortal/repository/OrderItemRepo.java
+++ src/main/java/com/example/bookPortal/repository/OrderItemRepo.java
@@ -3,5 +3,28 @@ package com.example.bookPortal.repository;
 import com.example.bookPortal.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface OrderItemRepo extends JpaRepository<OrderItem, Integer> {
+
+    List<OrderItem> findByOrder_OrderId(Integer orderId);
+
+    List<OrderItem> findByStoreBook_StoreBookId(Integer storeBookId);
+
+    List<OrderItem> findByStoreBook_Book_BookId(Integer bookId);
+
+    List<OrderItem> findByStoreBook_Store_StoreId(Integer storeId);
+
+    List<OrderItem> findByQuantityGreaterThan(Integer quantity);
+
+    List<OrderItem> findByBookPriceBetween(BigDecimal minPrice, BigDecimal maxPrice);
+
+    List<OrderItem> findBySubtotalBetween(BigDecimal minSubtotal, BigDecimal maxSubtotal);
+
+    List<OrderItem> findByStoreBook_Book_TitleContainingIgnoreCase(String title);
+
+    List<OrderItem> findByOrder_User_UserId(Integer userId);
+
+    List<OrderItem> findByOrder_OrderStatusIgnoreCase(String orderStatus);
 }
